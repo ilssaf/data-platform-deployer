6b326f03cd1990ee7f09284fb556e385456ae941
ilsaffff <justsairex@gmail.com>
2025-03-22 11:47:17 +0300

    feat: add support yaml and validation

--- src/dpd/models.py
+++ src/dpd/models.py
@@ -1,14 +1,71 @@
 from abc import ABC
 from enum import StrEnum
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Any
 from dataclasses import dataclass
 import json
+import yaml
+import jsonschema
 
 
-def load_config_from_json(json_path: str) -> "Config":
+def _validate_json(json_path: str, schema_path: str) -> bool:
     with open(json_path, "r") as f:
         conf_dict = json.load(f)
+    with open(schema_path, "r") as f:
+        schema = json.load(f)
+    try:
+        jsonschema.validate(conf_dict, schema)
+    except jsonschema.ValidationError as e:
+        print(f"🚨 Validation error: {e.message}")
+        return False
+    return True
 
+def _validate_yaml(yaml_path: str, schema_path: str = 'schema.json') -> bool:
+    with open(yaml_path, "r") as f:
+        conf_dict = yaml.safe_load(f)
+    with open(schema_path, "r") as f:
+        schema = yaml.safe_load(f)
+    try:
+        jsonschema.validate(conf_dict, schema)
+    except jsonschema.ValidationError as e:
+        print(f"🚨 Validation error: {e.message}")
+        return False
+    return True
+
+def validate(file_path: str, schema_path: str) -> bool:
+    if file_path.endswith(".json"):
+        return _validate_json(file_path, schema_path)
+    elif file_path.endswith(".yaml") or file_path.endswith(".yml"):
+        return _validate_yaml(file_path, schema_path)
+    else:
+        raise ValueError("Unknown file format. Must be either JSON or YAML.")
+
+
+def from_yaml_to_json(yaml_path: str) -> str:
+    with open(yaml_path, "r") as f:
+        conf_dict = yaml.safe_load(f)
+    return json.dump(conf_dict)
+
+
+def _load_config_from_yaml(yaml_path: str) -> "Config":
+    with open(yaml_path, "r") as f:
+        conf_dict = yaml.safe_load(f)
+    return _load_config(conf_dict)
+
+
+def _load_config_from_json(json_path: str) -> "Config":
+    with open(json_path, "r") as f:
+        conf_dict = json.load(f)
+    return _load_config(conf_dict)
+
+
+def load_config_from_file(file_path: str) -> "Config":
+    if file_path.endswith(".json"):
+        return _load_config_from_json(file_path)
+    elif file_path.endswith(".yaml") or file_path.endswith(".yml"):
+        return _load_config_from_yaml(file_path)
+
+
+def _load_config(conf_dict: Dict[str, Any]):
     project = Project(**conf_dict["project"])
 
     sources = []
